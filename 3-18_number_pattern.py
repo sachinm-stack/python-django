f0ba6767dae922f203e57734d444d24817482878
sachin <sachinmeganathan698@gmail.com>
2026-03-18 18:05:11 +0530

    First commit

---
+++ 3-18_number_pattern.py
@@ -0,0 +1,73 @@
+# print("print the loop 3 to 30:")
+# for  i in range (3,31,3):
+#     print(i)
+# print("reverse the loop 30 to 3:")
+# for i in range (30,2,-3):
+#     print(i)
+
+
+
+
+# for i in range(1,20):
+#     if i ==15:
+#         break
+#     if i  % 2 == 0:
+#         continue
+ 
+#     print(i)
+
+
+
+
+# list=[44,12,88,5,33,88,7]
+# list.append(99)
+# print(list)
+# list.insert(2,0)
+# print(list)
+# list.remove(88)
+# print(list)
+# list.sort()
+# print(list)
+# list.pop()
+# print(list)
+# print(list.count(88))
+# print(list.index(33))
+
+
+
+
+
+# visitors = ["alice","bob","alice","carol","bob","dave","alice"]
+# print(visitors.__len__())
+# unique=set(visitors)
+# print(unique)
+# print(unique.__len__())
+# if "eve" in visitors:
+#     print("yes",visitors)
+# else:
+#     print("no")
+# if "bob" in visitors:
+#     print("yes",visitors)
+# else:
+#     print("no")
+
+# import random
+# secret=random.randint(1,10)
+# attempt=3
+# while attempt>0:
+#     guess =int(input("guess number 1-10:"))
+#     if guess == secret:
+#         print("you Won!")
+#         break
+#     else :
+#         attempt -=1
+#         print("wrong!",attempt,"attempt left")
+# else:
+#     print("game over! number was:",secret)
+
+
+
+alice_friends = {"bob","carol","dave","eve"} 
+bob_friends = {"carol","frank","eve","grace"}
+
+
